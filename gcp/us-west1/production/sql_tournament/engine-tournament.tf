3939ed6c8a89f8e7ded824505200c8f806973fba
Matthieu Dolci <matthieu.dolci@gmail.com>
2019-02-27 11:27:42 -0800

    Add engine tournament db

---
+++ gcp/us-west1/production/sql_tournament/engine-tournament.tf
@@ -0,0 +1,80 @@
+module "postgresql-engine-tournament" {
+  source                      = "../../../../modules/gcp/sql"
+
+  project                     = "battlesnake-io"
+
+  name                        = "battlesnake-engine-tournament"
+  database_version            = "POSTGRES_9_6"
+  region                      = "us-west1"
+  tier                        = "db-custom-16-65536" # 16 CPU, 64GB ram
+  activation_policy           = "ALWAYS"
+  availability_type           = "REGIONAL"
+  disk_autoresize             = "true"
+  disk_size                   = "100"
+  disk_type                   = "PD_SSD"
+  backup_configuration        = [{
+    enabled = "true"
+    start_time = "00:00"
+  }]
+  maintenance_window          = [{
+    day = "7"
+    hour = "23"
+    update_track = "stable"
+  }]
+
+  db_name                     = "engine"
+  db_charset                  = "UTF8"
+  db_collation                = "en_US.UTF8"
+
+}
+
+module "db-user-engine-tournament" {
+  source    = "../../../../modules/gcp/sql_user"
+
+  project   = "battlesnake-io"
+
+  user_name = "proxyuser"
+  instance = "${module.postgresql-engine-tournament.instance_name}"
+  
+}
+
+module "cloudsqlproxy-engine-tournament-service-account" {
+  source        = "../../../../modules/gcp/service_account"
+  
+  account_id    = "cloudsqlproxy-enginetournament"
+  display_name  = "cloudsqlproxy-enginetournament"
+  project       = "battlesnake-io"
+}
+
+module "cloudsqlproxy-engine-tournament-service-account-key" {
+  source = "../../../../modules/gcp/service_account_key"
+
+  service_account_id = "${module.cloudsqlproxy-engine-tournament-service-account.name}"
+}
+
+module "cloudsqlproxy-engine-tournament-binding-cloudsql-client" {
+  source                = "../../../../modules/gcp/project_iam_binding"
+
+  project               = "battlesnake-io"
+
+  role                  = "roles/cloudsql.client"
+  members               = ["serviceAccount:${module.cloudsqlproxy-engine-tournament-service-account.email}"]
+}
+
+module "cloudsqlproxy-engine-tournament-binding-cloudsql-editor" {
+  source                = "../../../../modules/gcp/project_iam_binding"
+  
+  project               = "battlesnake-io"
+
+  role                  = "roles/cloudsql.editor"
+  members               = ["serviceAccount:${module.cloudsqlproxy-engine-tournament-service-account.email}"]
+}
+
+module "cloudsqlproxy-engine-tournament-binding-cloudsql-admin" {
+  source                = "../../../../modules/gcp/project_iam_binding"
+
+  project               = "battlesnake-io"
+
+  role                  = "roles/cloudsql.admin"
+  members               = ["serviceAccount:${module.cloudsqlproxy-engine-tournament-service-account.email}"]
+}
---
+++ gcp/us-west1/production/sql_tournament/main.tf
@@ -0,0 +1,14 @@
+provider "google" {
+  region  = "us-west1"
+  project = "battlesnake-io"
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "terraform-battlesnake-io"
+    prefix = "battlesnake/sql-tournament"
+  }
+
+  required_version = "= 0.11.7"
+}
+
